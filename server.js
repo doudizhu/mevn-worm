5237f2d8f07ca91c2eb506e89f01a2bbf6c80081
lipeng <lipeng@daokoudai.com>
2019-08-19 15:15:16 +0800

    make it better

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/dd',(req,res)=>{
 
 // 执行前端静态页面
 if(process.env.NODE_ENV==='production'){
-  app.use(express.static('client'))
+  app.use(express.static('client/dist'))
   app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'client','dist','index.html'))
   })
