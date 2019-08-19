89baa9ff61aa388b54c6e200aa5aaf3d39940637
lipeng <lipeng@daokoudai.com>
2019-08-19 18:15:03 +0800

    v0.0.9 项目配置v0.1 请求插件

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,9 +4,16 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import request from './utils/request'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.prototype.$request = request
+declare module 'vue/types/vue' { // 可简写（this as any）
+  interface Vue {
+    $request: any;
+  }
+}
 
 new Vue({
   router,
