3d066bae07ee5f46b1a77c3d86b6f2957b8a0854
lipeng <lipeng@daokoudai.com>
2019-08-19 15:05:16 +0800

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.get('/',(req,res)=>{
   res.send('Hello World!')
 })
+
+// 执行前端静态页面
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static('client'))
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','dist','index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000;
 
