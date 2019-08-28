5e129fc6fde2efcf4d4f199998dd8607695fea06
lipeng <lipeng@daokoudai.com>
2019-08-28 14:16:45 +0800

    v0.1.8 登录页面：token持久化

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import {routes} from './router'
+import createPersistedState from 'vuex-persistedstate' // vuex数据持久化
 
 Vue.use(Vuex)
 
@@ -14,6 +15,8 @@ const stateTpl = {
    * http://goodhope-spider-manage.herokuapp.com/api/extend
    */
   domainApi:`${process.env.NODE_ENV==='production' ? '' : 'http://localhost:5000'}/api`,
+  /**登录相关 */
+  eleToken:'',
 }
 
 export default new Vuex.Store({
@@ -24,10 +27,21 @@ export default new Vuex.Store({
     SET_ROUTES(state,routes){
       state.routes = routes;
     },
+    // 重置vuex,相对于刷新跳转清空体验好
+    resetAll(state) {
+      Object.assign(state, stateTpl) // 要用Object.assign而不是直接给state赋值。  
+    },
+    
+    setEleToken(state, eleToken){ 
+      state.eleToken = eleToken
+    },
   },
   actions: {
     async setRoutes({state,commit},user){
       commit('SET_ROUTES',routes)
     },
   },
+  plugins: [
+    createPersistedState(),
+  ],
 })
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -37,6 +37,9 @@ export default class ViewComponent extends Vue {
         message:'登录成功',
         type: 'success'
       })
+      if(response.data.success === true){
+        this.$store.commit('setEleToken',response.data.token)
+      }
       this.$router.push ('/')
     }else{
       // * 待优化：处理错误返回结果
